5db4be15e7ec532f61badabafcbb80ba5b1e7e7e
Mario <mario.espasa.2003@gmail.com>
2022-10-10 11:09:25 +0200

    Actualizacion

--- ThemeVariety/app/src/main/java/espasa/mario/themevariety/MainActivity.kt
+++ ThemeVariety/app/src/main/java/espasa/mario/themevariety/MainActivity.kt
@@ -1,50 +1,43 @@
-package espasa.mario.filmoteca2
+package espasa.mario.themevariety
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import espasa.mario.filmoteca2.ui.theme.Filmoteca2Theme
+import espasa.mario.themevariety.ui.theme.ThemeVarietyTheme
 
-class AboutActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Filmoteca2Theme {
+            ThemeVarietyTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    DefaultPreview()
+                    Greeting("Android")
                 }
             }
         }
     }
 }
-@Composable
-fun myImage(){
-    Image(painter = painterResource(id = R.drawable.mario), contentDescription = "Image")
-}
 
 @Composable
-fun Welcome(name: String) {
-    Text(text = "Welcome to my new app I'm $name! An student from IES Pere Maria Orts i Bosch", style = MaterialTheme.typography.body1)
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Filmoteca2Theme {
-        Welcome("Mario")
-        myImage()
+    ThemeVarietyTheme {
+        Greeting("Android")
     }
 }
