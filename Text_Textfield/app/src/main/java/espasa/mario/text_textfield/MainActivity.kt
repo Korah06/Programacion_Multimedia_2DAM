7cdc1fbd120259ead263d8d85a582db9f6a19438
Mario <mario.espasa.2003@gmail.com>
2022-10-11 17:25:28 +0200

    actualizacion

---
+++ Text_Textfield/app/src/main/java/espasa/mario/text_textfield/MainActivity.kt
@@ -0,0 +1,43 @@
+package espasa.mario.text_textfield
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import espasa.mario.text_textfield.ui.theme.Text_TextfieldTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Text_TextfieldTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Text_TextfieldTheme {
+        Greeting("Android")
+    }
+}
